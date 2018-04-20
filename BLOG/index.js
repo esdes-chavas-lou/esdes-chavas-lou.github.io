1fc471004884902599d9702c7515dbfa7cfcb803
esdes-chavas-lou <32122362+esdes-chavas-lou@users.noreply.github.com>
2018-04-20 12:40:07 +0200

    Delete index.js

--- BLOG/index.js
+++
@@ -1,9 +0,0 @@
-var lineDrawing = anime({
-    targets: 'path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 1500,
-    delay: 200,
-    direction: 'alternate',
-    loop: true
-  });
